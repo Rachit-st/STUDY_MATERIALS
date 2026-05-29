e1b2926d914a1ac4d288e5c35b57cd4cb3159395
Rachit-st <rachitomar17@gmail.com>
2026-05-30 01:52:51 +0530

    File On How To Add First n Numbers, 4 Was The Number Here

---
+++ JavaBasics/5_firstnsum.java
@@ -0,0 +1,24 @@
+class FirstNSum {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 5; i=i+1) {
+            a = a + i;
+        }
+        System.out.print(a+ " ");
+        int h = 0;
+        int b = 0;
+        while(h<4){
+            h = h + 1;
+            b = b + h;
+        }
+        System.out.print(b+ " ");
+        int f = 0;
+        int c = 0;
+        do{
+            f=f+1;
+            c=c+f;
+        }while(f<4);
+        System.out.print(c);
+
+    }
+}
