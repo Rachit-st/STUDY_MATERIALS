cec3ad57490393cd74de097af52959c75921e9b6
Rachit-st <rachitomar17@gmail.com>
2026-05-30 00:17:09 +0530

    added new file for test

---
+++ JavaBasics/whileloop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+class whileloop {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int i= 0;
+        while (i<11){
+            System.out.println(i+" ");
+            i++;
+        }
+    }
+}
