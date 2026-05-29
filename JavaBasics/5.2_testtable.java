9587df3cefdfe76c5d4e28837abf6c57bd76c233
Rachit-st <rachitomar17@gmail.com>
2026-05-30 02:14:38 +0530

    added file and managed index

---
+++ JavaBasics/5.2_testtable.java
@@ -0,0 +1,10 @@
+class testtable {
+    public static void main(String[] args){
+        int a = 0;
+        for(int i = 0; i < 11; i= i + 1){
+            a = i * 2;
+            System.out.println(a);
+        }
+        
+    }
+}
