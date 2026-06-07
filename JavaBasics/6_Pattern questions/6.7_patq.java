17b3e2020e600541247e3e0b123babfc7afa2fea
Rachit-st <rachitomar17@gmail.com>
2026-06-07 15:33:26 +0530

    Completed all basic patter question and added them

---
+++ JavaBasics/6_Pattern questions/6.7_patq.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class patq7{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter columns:");
+        int n = input.nextInt();
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<= n - i + 1; j++){
+                System.out.print(j);
+            }
+        System.out.println();    
+        }
+    }
+}
---
+++ JavaBasics/6_Pattern questions/6.8_patq.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class patq8{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter columns:");
+        int n = input.nextInt();
+        int m = 0;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<= i; j++){
+                m=m+1;
+                System.out.print(m + " ");
+            }
+        System.out.println();
+        }
+    }
+}
